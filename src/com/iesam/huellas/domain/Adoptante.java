5f5ab7c8328550fa702e049c973e3936a9a1c30b
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-02-20 12:13:53 +0100

    Creando las clases

--- src/com/iesam/huellas/domain/Adoptante.java
+++ src/com/iesam/huellas/domain/Adoptante.java
@@ -1,35 +1,10 @@
 package com.iesam.huellas.domain;
 
-public class Adoptante {
-    private String id;
-    private String name;
-    private String dni;
+public class Adoptante extends Persona{
+
     private String fechaNacimiento;
     private String direccion;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
 
     public String getFechaNacimiento() {
         return fechaNacimiento;
---
+++ src/com/iesam/huellas/domain/Persona.java
@@ -0,0 +1,40 @@
+package com.iesam.huellas.domain;
+
+public class Persona {
+    protected String id;
+    protected String name;
+    protected String apellidos;
+    protected String dni;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
